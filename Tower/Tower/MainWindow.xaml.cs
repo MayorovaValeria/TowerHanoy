2f6282f882c9d9631ba95456719799caea946e08
Майорова Валерия Александровна <62285970+MayorovaValeria@users.noreply.github.com>
2020-05-25 19:40:53 +0400

    Add files via upload

---
+++ Tower/Tower/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tower
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Panel fromTower = null;
+        Brush noActive = new SolidColorBrush(Color.FromArgb(0, 255, 255, 255));
+        Brush Active = new SolidColorBrush(Color.FromArgb(128, 0xCC, 0xCC, 0xCC));
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void tossdisk_GotFocus(object sender, MouseEventArgs e)
+        {
+            if (!(sender is Panel))
+                return;
+            var toTower = (Panel)sender;
+            if (fromTower == null)
+            {
+                if (toTower.Children.Count > 0)
+                {
+                    fromTower = (Panel)sender;
+                    fromTower.Background = Active;
+                }
+            }
+            else if (toTower == fromTower)
+            {
+                fromTower.Background = noActive;
+                fromTower = null;
+            }
+            else
+            {
+                var disk = fromTower.Children[fromTower.Children.Count - 1];
+                if (toTower.Children.Count == 0 || (disk as Shape).Width < (toTower.Children[toTower.Children.Count - 1] as Shape).Width)
+                {
+                    fromTower.Children.Remove(disk);
+                    toTower.Children.Add(disk);
+                    fromTower.Background = noActive;
+                    fromTower = null;
+                }
+                else
+                {
+                    MessageBox.Show("Неверный ход!");
+                }
+            }
+        }
+
+
+    }
+}
